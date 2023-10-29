8b5e645933915c25d00635076088ed9f158bfbe5
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-10-29 19:37:55 +0600

    signup form added

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -14,7 +14,7 @@ console.log(email,password);
         <div className="hero min-h-screen bg-base-200">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
+            <h1 className="text-5xl font-bold">Sign In Now!</h1>
               </div>
           <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <form onSubmit={handleSignIn} className="card-body">
